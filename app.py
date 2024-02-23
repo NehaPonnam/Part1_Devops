e007ca3bce44860fd1dbc0929a2a7d16625a9372
Neha Ponnam <neha.ponnam05@gmail.com>
2024-02-23 23:43:37 +0530

    welcome

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    print("Hello")
     return '<h1>Hello from Flask & Docker develoop changes</h2>'
 
 
 if __name__ == "__main__":
+    print("Hello")
+
     app.run(debug=True)
